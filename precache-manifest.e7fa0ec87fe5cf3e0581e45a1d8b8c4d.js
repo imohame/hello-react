08fa267532393e868f2d4e7cdef5d468c00a74bb
ismail <ismail2005@gmail.com>
2020-10-12 11:49:02 -0400

    Updates

---
+++ precache-manifest.e7fa0ec87fe5cf3e0581e45a1d8b8c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe13570eb19e57c752057988bfd24a79",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "bfc43f9c849b85b26564",
+    "url": "/hello-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2a921adb50cfa101f94",
+    "url": "/hello-react/static/js/2.222e0358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.222e0358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc43f9c849b85b26564",
+    "url": "/hello-react/static/js/main.e433b4c1.chunk.js"
+  },
+  {
+    "revision": "61b7d1b2978b87885f93",
+    "url": "/hello-react/static/js/runtime-main.1dbbaa17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
